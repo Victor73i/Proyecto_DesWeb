1caa82bf65f9518726d6ac9f66229a409787ae9e
Luis Villatoro <lvillatorom4@miumg.edu.gt>
2022-10-08 11:30:19 -0600

    Rechazado

---
+++ database/migrations/2022_10_08_155107_create_sucursal_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSucursalTable1 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sucursal', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_sucursal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sucursal');
+    }
+}
