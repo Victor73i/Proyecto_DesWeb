5e1d6d2a5c965b6bf62622a916fe78d5dae29dc2
Victor73i <deleonvictor839@gmail.com>
2022-10-07 22:07:59 -0600

    pequeños cambios

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,3 +1,18 @@
 const Home = () => import('./components/Home.vue')
 
 
+
+
+export const routes = [
+
+    {
+        name: 'home',
+        path: '/',
+        component: Home
+    },
+
+
+
+
+
+]
